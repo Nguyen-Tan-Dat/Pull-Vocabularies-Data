c33a1affbb3be128442a40f69b7106418c6d47e9
Dat Nguyen <dat.nguyen@dvinci.com.vn>
2024-03-26 17:42:57 +0700

    update

--- src/main/java/example/Request.java
+++ src/main/java/example/Request.java
@@ -1,21 +1,171 @@
 package example;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-public class Request {
-    public static void main(String[] args) throws IOException, InterruptedException {
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
-        URL url = new URL("https://api.api-ninjas.com/v1/dictionary?word=code");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestProperty("accept", "application/json");
-        InputStream responseStream = connection.getInputStream();
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(responseStream);
-        System.out.println(root.path("fact").asText());;
+public class Request {
+
+    public static String pronunciationsDictionary(String word) {
+        String pronunciation = "";
+        String apiUrl = "https://dict.laban.vn/ajax/getsound?accent=us&word=" + word;
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        try {
+            URI uri = new URI(apiUrl);
+            HttpGet httpGet = new HttpGet(uri);
+            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode == 200) {
+                    String responseBody = EntityUtils.toString(response.getEntity());
+                    pronunciation = new JSONObject(responseBody).getString("data");
+                }
+            }
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return pronunciation;
+    }
+
+    public static void main(String[] args) {
+
+    }
+    public static String phonetic(String word){
+        String phonetic = "";
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        try {
+            URI uri = new URI("https://dict.laban.vn/ajax/widget-search?type=1&query="+word+"&vi=0");
+            HttpGet httpGet = new HttpGet(uri);
+
+            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode == 200) {
+                    String responseBody = EntityUtils.toString(response.getEntity());
+                    System.out.println(responseBody);
+                    String data=new JSONObject(responseBody).getJSONObject("enEnData").getJSONObject("best").getString("details");
+                    Document document = Jsoup.parse(data);
+
+                    // Lấy phần tử span có class là color-black để lấy phát âm
+                    Elements spanElements = document.select("span.color-black");
+                    for (Element span : spanElements) {
+                        phonetic = span.text();
+                        break; // Chỉ lấy phát âm đầu tiên
+                    }
+                }
+            }
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return phonetic;
+    }
+    static class Result {
+        private String name;
+        private List<String> words;
+
+        public Result(String name, List<String> words) {
+            this.name = name;
+            this.words = words;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public List<String> getWords() {
+            return words;
+        }
+    }
+    public static String partOfSpeech(String word){
+        String apiKey = "pvi0hwxptqvz5t4fbyz51dcz8l8ivfj2jalnhjj8b2gt1k13w";
+        String  partOfSpeech="";
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            // Xây dựng URL cho yêu cầu API
+            URI uri = new URI("https", "api.wordnik.com", "/v4/word.json/" + word + "/definitions", "api_key=" + apiKey, null);
+            HttpGet httpGet = new HttpGet(uri);
+
+            // Thực hiện yêu cầu GET và nhận phản hồi
+            try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+                // Xử lý phản hồi
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode == 200) {
+                    // Lấy nội dung phản hồi
+                    String responseBody = EntityUtils.toString(response.getEntity());
+                    JSONArray jsonObject = new JSONArray(responseBody);
+                    if (jsonObject.length() > 0) {
+                        JSONObject firstPhoto = jsonObject.getJSONObject(0);
+                        partOfSpeech = firstPhoto.getString("partOfSpeech");
+
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (URISyntaxException | IOException e) {
+            e.printStackTrace();
+        }
+        return partOfSpeech;
+    }
+    public static String image(String word){
+        String apiKey = "3cXnXWgieuriCD5oOmEM6SSepoFTrUs6A7OqK2IG7y1dkg96QFGcLenH";
+
+        // URL của API
+        String apiUrl = "https://api.pexels.com/v1/search?query=" + word;
+
+        // Tạo HttpClient
+        HttpClient httpClient = HttpClients.createDefault();
+
+        // Tạo HttpGet request
+        HttpGet httpGet = new HttpGet(apiUrl);
+
+        // Thiết lập Authorization header
+        httpGet.setHeader("Authorization", apiKey);
+        String image="";
+        try {
+            // Gửi request và nhận phản hồi
+            HttpResponse response = httpClient.execute(httpGet);
+
+            // Đọc nội dung phản hồi
+            String responseBody = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+            JSONObject jsonObject = new JSONObject(responseBody);
+            JSONArray photosArray = jsonObject.getJSONArray("photos");
+
+            // Check if there are any photos
+            if (photosArray.length() > 0) {
+                JSONObject firstPhoto = photosArray.getJSONObject(0);
+                image = firstPhoto.getJSONObject("src").getString("original");
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
     }
 }
--- src/main/java/example/Test.java
+++ src/main/java/example/Test.java
@@ -6,7 +6,7 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -70,7 +70,61 @@ public class Test {
 //        partArrayCode("sft_lhfa_boys_z_0_5.xlsx");
 //        partArrayCode("sft_lhfa_girls_z_0_5.xlsx");
 //    }
+    static class Vocab{
+        private int en,vi,to;
+
+        public Vocab(int en, int vi, int to) {
+            this.en = en;
+            this.vi = vi;
+            this.to = to;
+        }
+
+        public int getEn() {
+            return en;
+        }
+
+        public void setEn(int en) {
+            this.en = en;
+        }
+
+        public int getVi() {
+            return vi;
+        }
+
+        public void setVi(int vi) {
+            this.vi = vi;
+        }
+
+        public int getTo() {
+            return to;
+        }
+
+        public void setTo(int to) {
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            Vocab vocab = (Vocab) o;
+            return en == vocab.en && vi == vocab.vi;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(en, vi, to);
+        }
+    }
     public static void main(String[] args) {
+        int id=2;
+        String topic="No name";
+        HashMap<String,Integer> topics=new HashMap<>();
+        HashMap<String,Integer> english=new HashMap<>();
+        int enMax=1;
+        int viMax=1;
+        HashMap<String,Integer> vietnamese=new HashMap<>();
+        Set<String> vocab=new HashSet<>();
+        Set<Vocab> vList=new HashSet<>();
         byte count = 0;
         byte countTopic = 0;
         int countVocab = 0;
@@ -82,11 +136,14 @@ public class Test {
 
                 if (!containsNumber(line)) {
                     count++;
-                    System.out.println(line);
+//                    System.out.println(line);
 //                    writer=new BufferedWriter(new FileWriter("data_group"+count+".txt"));
                 } else {
                     if (!startsWithDigit(line)) {
-                        System.out.println(line);
+                        topic=line;
+//                        System.out.println(line);
+                        id++;
+                        topics.put(line,id);
                         countTopic++;
                     } else {
                         String regex = "(\\d+\\.\\s.*?)(?=(\\d+\\.\\s|$))";
@@ -97,17 +154,19 @@ public class Test {
                             result.add(matcher.group(1));
                         }
                         for (String item : result) {
+                            String en="";
+                            String vi="";
 //                            System.out.println(item.replaceFirst(". ", ".\t"));
 //                            if(item.trim().contains(". "))
                             String row = item.toLowerCase().trim().replaceFirst(" ", "\t");
-                            row = removeNumberAndDot(row).replaceAll("\t", "");
+                            row = removeNumberAndDot(row).replaceFirst("\t", " ");
                             String[] parts = row.split("\\s*/\\s*");
                             if (parts.length > 3) {
                                 String e=parts[0].replaceAll(":", "").replace(".", "").trim();
                                 if (isEnglish(e)) {
-                                    row = e + " => " + parts[parts.length - 1].replaceAll(":","");
                                     countVocab++;
-                                    System.out.println(row);
+                                    en=e;
+                                    vi= parts[parts.length - 1].replaceAll(":","");
                                 }
 //                                else {
 //                                    countVocab++;
@@ -116,37 +175,61 @@ public class Test {
                             } else if (parts.length == 3) {
                                 String e=parts[0].replaceAll(":", "").trim();
                                 if (isEnglish(e)) {
-                                    row = e + " => " + parts[2].replaceAll(":","");
+                                    en=e;
+                                    vi=parts[2].replaceAll(":","");
                                     countVocab++;
-                                    System.out.println(row);
                                 }
                                 else {
-                                    row = parts[2] + " => " + parts[0];
+                                    en = parts[2] ;
+                                    vi=parts[0];
                                     countVocab++;
-                                    System.out.println(row);
                                 }
 
                             } else {
                                 parts = row.split(":");
                                 if (parts.length > 2) {
-                                    row = parts[0].trim() + " => " + parts[parts.length-1].trim();
-                                    System.out.println(row);
+                                    en=parts[0].trim();
+                                    vi=parts[parts.length-1].trim();
                                     countVocab++;
                                 }
                                 else if (parts.length == 2) {
-                                    row = parts[0].trim() + " => " + parts[1].trim();
-                                    System.out.println(row);
+                                    en=parts[0].trim();
+                                    vi=parts[1].trim();
                                     countVocab++;
                                 } else {
                                     parts = row.split("\\(.*?\\)");
                                     if (parts.length == 2) {
-                                        row = parts[0].trim() + " => " + parts[1].trim();
-                                        System.out.println(row);
+                                        en=parts[0].trim();
+                                        vi=parts[1].trim();
                                         countVocab++;
                                     }
                                 }
 
                             }
+                            en=removeParenthesesContent(en).trim();
+                            en=en.split("/")[0];
+                            int enID=0;
+                            if(english.containsKey(en)){
+                                enID=english.get(en);
+                            }else {
+                                enID=enMax;
+                                english.put(en,enMax++);
+                            }
+                            vi=removeParenthesesContent(vi).trim();
+                            int viID=0;
+                            if(vietnamese.containsKey(vi)){
+                                viID=vietnamese.get(vi);
+                            }else {
+                                viID=viMax;
+                                vietnamese.put(vi,viMax++);
+                            }
+                            if(vocab.contains(enID+","+viID)) {
+//                                System.out.println(enID);
+                            }else{
+                                vList.add(new Vocab(enID,viID,id));
+                                vocab.add(enID+","+viID);
+                            }
+//                            System.out.println(en+" => "+vi);
                         }
                     }
 
@@ -158,8 +241,42 @@ public class Test {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        for(var i:topics.keySet()){
+            System.out.println("INSERT INTO topics value ("+topics.get(i)+",'"+i+"',1);");
+        }
+        for (var i:english.keySet()){
+            System.out.println("INSERT INTO english value ("+english.get(i)+",'"+i+"','','');");
+        }
+        for (var i:vietnamese.keySet()){
+            System.out.println("INSERT INTO vietnamese value ("+vietnamese.get(i)+",'"+i+"');");
+        }
+        int voID=1;
+        for (var i:vList){
+            String en="";
+            for (var j:english.keySet()){
+                if(i.en==english.get(j))en=j;
+            }
+            String vi="";
+            for (var k:vietnamese.keySet()){
+                if(i.vi==vietnamese.get(k))vi=k;
+            }
+            System.out.println("INSERT INTO vocabularies (id,en,part_of_speech,img,vi,user) VALUE ("+voID+","+i.getEn()+",'noun','',"+i.getVi()+",1);");
+            System.out.println("INSERT INTO vocabularies_topics value ("+i.to+","+voID+");");
+            voID++;
+        }
     }
+    public static String removeParenthesesContent(String input) {
+        // Tạo mẫu regular expression
+        Pattern pattern = Pattern.compile("\\([^\\)]+\\)");
 
+        // Tạo một Matcher để tìm kiếm chuỗi trong ngoặc đơn
+        Matcher matcher = pattern.matcher(input);
+
+        // Thực hiện thay thế chuỗi trong ngoặc đơn bằng chuỗi trống
+        String result = matcher.replaceAll("").trim();
+
+        return result;
+    }
     private static boolean containsNumber(String line) {
         for (char c : line.toCharArray()) {
             if (Character.isDigit(c)) {
